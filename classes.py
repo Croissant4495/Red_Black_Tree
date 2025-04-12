d42a893cd9f0d1a616c1a60b02d710c875bea360
Abdelrahman Ahmed Hafez <Abdelrahmanelshabiny@gmail.com>
2025-04-13 01:11:09 +0200

    Finished insertion (Still fixup)

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.parent = None
-        self.left_child = None
-        self.right_child = None
-        self.color = 0                  # 0 -> Black , 1 -> Red
+        self.parent:Node = None
+        self.left_child:Node = None
+        self.right_child:Node = None
+        self.color = 1                  # 0 -> Black , 1 -> Red
 
     def set_left_child(self, child):
         self.left_child = child
@@ -18,16 +18,54 @@ class Node:
     def toggle_color(self):
         self.color = not self.color
 
-class Tree:
-    def __init__(self):
-       self.root = None
+class RedBlackTree:
+    nill = Node(None)
     
+    def __init__(self):
+       self.root = RedBlackTree.nill
+       self.nillify(self.root)
+    
+    def nillify(self, node):
+        node.set_left_child(RedBlackTree.nill)
+        node.set_right_child(RedBlackTree.nill)
+        node.set_parent(RedBlackTree.nill)
+
     def search(self, value):
+        pass
+
+    def fix_up(self, node:Node):
         pass
 
     def insert(self, value):
         if self.search(value):
             return
         new = Node(value)
+        self.nillify(new)
+
+        parent = RedBlackTree.nill
+        current = self.root
         
+        while current != RedBlackTree.nill:
+            parent = current
+            if value > current.value:
+                current = current.right_child
+            else:
+                current = current.left_child
+        
+        if parent == RedBlackTree.nill:
+            self.root = new
+        else:
+            if value > parent.value:
+                parent.right_child = new
+            else:
+                parent.left_child = new 
+            new.parent = parent
+        
+        self.fix_up(new)
+
+    def traverse(self, node:Node):
+        if node != RedBlackTree.nill:
+            self.traverse(node.left_child)
+            print(node.value)
+            self.traverse(node.right_child)
         
