2c3e78012d4ecafa26d43114289baa32abd7a254
youssef <youssefyoussef.m4@gmail.com>
2025-04-17 02:13:28 +0200

    Update RBT.py

--- RBT.py
+++ RBT.py
@@ -2,7 +2,7 @@ from classes import *
 
 def get_tree_height(root):
     if root is RedBlackTree.nill:
-        return -1
+        return 0
     left_height = get_tree_height(root.left_child)
     right_height = get_tree_height(root.right_child)
     return 1 + max(left_height, right_height)
@@ -11,7 +11,7 @@ def get_tree_height(root):
 def get_black_height(root):
     if root is RedBlackTree.nill:
         return 1
-    if root.color == 0:
+    if root.color == 0 and root.parent != RedBlackTree.nill:
         return 1 + get_black_height(root.left_child)
     else:
         return get_black_height(root.left_child)
@@ -22,14 +22,16 @@ def get_size(root):
         return 0
     return 1 + get_size(root.left_child) + get_size(root.right_child)
 
-#tempTree = RedBlackTree()
-#tempTree.insert(5)
-#tempTree.insert(6)
-#tempTree.insert(1)
-#tempTree.insert(8)
-#tempTree.insert(2)
-#tempTree.insert(3)
-
-#print(get_tree_height(tempTree.root))
-#print(get_black_height(tempTree.root))
-#print(get_size(tempTree.root))
+tempTree = RedBlackTree()
+tempTree.insert(10)
+tempTree.insert(1)
+tempTree.insert(12)
+tempTree.insert(4)
+tempTree.insert(8)
+tempTree.insert(19)
+tempTree.insert(2)
+tempTree.insert(21)
+tempTree.preorder_traversal(tempTree.root)
+print(get_tree_height(tempTree.root))
+print(get_black_height(tempTree.root))
+print(get_size(tempTree.root))
